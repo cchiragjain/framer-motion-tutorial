1d72eadb002f77ca3a8dab1d4b8d9fa07da47486
Chirag Jain <cjain8060@gmail.com>
2024-03-17 23:45:05 +0530

    Covered Basics

--- components/BasicsOfMotion.js
+++ components/BasicsOfMotion.js
@@ -1,5 +1,5 @@
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
 
 const BasicsOfMotion = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -19,33 +19,31 @@ const BasicsOfMotion = () => {
       >
         Show/Hide
       </motion.button>
+      {/* Pop Layout Removes Element from DOM */}
+      {/* AnimatePresence is required whenever we want to use exit animations */}
       <AnimatePresence mode="popLayout">
         {isVisible && (
           <motion.div
-            initial={{
-              rotate: "0deg",
-              scale: 0,
-              y: 0,
-            }}
-            animate={{
-              rotate: "180deg",
-              scale: 1,
-              y: [0, 150, -150, -150, 0],
-            }}
-            exit={{
-              rotate: "0deg",
-              scale: 0,
-              y: 0,
-            }}
-            transition={{
-              duration: 1,
-              ease: "backInOut",
-              times: [0, 0.25, 0.5, 0.85, 1],
-            }}
             style={{
               width: 150,
               height: 150,
               background: "black",
+            }}
+            initial={{
+              rotate: "0deg",
+              scale: 0,
+            }}
+            exit={{
+              rotate: "0deg",
+              scale: 0,
+            }}
+            animate={{
+              rotate: "180deg",
+              scale: 1,
+            }}
+            transition={{
+              duration: 1,
+              ease: "backInOut",
             }}
           ></motion.div>
         )}
