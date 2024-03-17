21c816b78b41e15b2a5a214c062a5fe879ff910a
Chirag Jain <cjain8060@gmail.com>
2024-03-18 00:07:29 +0530

    Added motion config and gestures

--- components/Gestures.js
+++ components/Gestures.js
@@ -1,3 +1,4 @@
+import { motion, MotionConfig } from "framer-motion";
 import React from "react";
 
 const Gestures = () => {
@@ -9,7 +10,31 @@ const Gestures = () => {
         height: "100vh",
         gap: "0.8rem",
       }}
-    ></div>
+    >
+      {/* Define single config of transition which can be used by the child components */}
+      <MotionConfig
+        transition={{
+          duration: 0.25,
+          ease: "easeIn",
+        }}
+      >
+        <motion.button
+          className="example-button"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95, rotate: "3deg" }}
+        >
+          click me
+        </motion.button>
+        <motion.button
+          className="example-button"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95, rotate: "-3deg" }}
+          style={{ backgroundColor: "red" }}
+        >
+          click me
+        </motion.button>
+      </MotionConfig>
+    </div>
   );
 };
 
